44ea9f7f20b4887297f0fbb207a0f0f7223496db
Gaurav Nagar <gaurav.nagar@thetrainline.com>
2017-01-24 15:53:55 +0000

    Added new routes to get news artices based on category and source

--- src/server/routes/components/news/news-api.js
+++ src/server/routes/components/news/news-api.js
@@ -1,6 +1,8 @@
 const fetch = require('node-fetch');
 const _ = require('lodash');
 const newsSourcesUrl = 'https://newsapi.org/v1/sources';
+const newsApiArticleSourceUrl ='https://newsapi.org/v1/articles?';
+const newsApiapiKey ='f9cd95f7d2714738a4d2baed0c7af52f';
 
 let newsSources = {};
 const getNewsSources = (res) => {
@@ -23,9 +25,37 @@ const getNewsCategories = (res) => {
         fetch(newsSourcesUrl)
             .then((res) => res.json())
             .then((json) => {
-                res.json(_.uniqBy(_.map(json.sources, 'category')));
+                res.json((json.sources.map((source) => {
+                    var obj = {};
+                    obj['id'] = source.id;
+                    obj['category'] = source.category;
+                    return obj;
+                })));
             });
     }
 };
 
-module.exports = {getNewsSources, getNewsCategories};
+const getNewsByCategory = (res, category) => {
+    fetch(newsSourcesUrl)
+        .then((res) => res.json())
+        .then((json) => {
+            res.json(_.filter(json.sources, (news) => { return news.category === category}))
+        })
+};
+
+const getNewsBySourceAndCategory = (res, params) => {
+    const url= `${newsApiArticleSourceUrl}&source=${params['source']}&category=${params['category']}&apiKey=${newsApiapiKey}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((json) => {
+            console.log('response:',json);
+            res.json(_.first(json.articles));
+        })
+};
+
+module.exports = {
+    getNewsSources,
+    getNewsCategories,
+    getNewsByCategory,
+    getNewsBySourceAndCategory
+};
