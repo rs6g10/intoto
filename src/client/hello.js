4167782c864b6f348133e21a737c4490b16cc486
Gaurav Nagar <gaurav.nagar@thetrainline.com>
2017-02-15 00:54:13 +0000

    refactoring

--- src/client/hello.js
+++ src/client/hello.js
@@ -4,33 +4,48 @@ import DisplayWeather from "./weather";
 export default class Hello extends React.Component {
     constructor(props) {
         super(props);
+        this.cities = ['london', 'paris', 'oslo'];
         //set initialState
         this.state = {
-            londonWeatherData: undefined
+            londonWeatherData: undefined,
+            parisWeatherData: undefined,
+            osloWeatherData: undefined,
         };
-        this.fetchAsync('api/weather/london');
+
+        this.cities.forEach((city) => {
+            this.fetchAsync(`api/weather/${city}`,`${city}WeatherData`);
+        });
+        // this.fetchAsync('api/weather/london', 'londonWeatherData');
+        // this.fetchAsync('api/weather/paris', 'parisWeatherData');
+        // this.fetchAsync('api/weather/oslo', 'osloWeatherData');
     }
 
-    async fetchAsync(url) {
+    async fetchAsync(url, propName) {
         const response = await(fetch(url));
         if (response.status >= 300) {
             throw new Error('invalid response');
         } else {
             const responseJson = await response.json();
-            this.setState({londonWeatherData: responseJson});
+            this.setState({[propName]: responseJson});
             return responseJson;
         }
     }
 
     render() {
-        const londonWeatherDataMessage = this.state.londonWeatherData;
+            debugger;
+        // const londonWeatherDataMessage = this.state.londonWeatherData;
+        // const parisWeatherDataMessage = this.state.parisWeatherData;
+        // const osloWeatherDataMessage = this.state.osloWeatherData;osloWeatherData
         return (
-            <div>
-                <div>
-                    {londonWeatherDataMessage
-                        ? <DisplayWeather weatherData={londonWeatherDataMessage}/> :
-                        <h1>Loading...</h1>}</div>
-            </div>
-        );
+        <div>
+            {
+                this.cities.map((city,idx)=> {
+                    <div key={idx} className="col-6 col-sm-4">
+                        {this.state[`${city}WeatherData`]
+                            ? <DisplayWeather weatherData={this.state[`${city}WeatherData`]}/> :
+                            <h1>Loading...</h1>}</div>
+            })
+            }
+        </div>);
     }
 }
